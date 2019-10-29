9421adbf8897f9584b57dd6dad868ed06db3e9d5
B3W <westieberg@gmail.com>
2019-10-29 13:03:42 -0500

    Added new constants

--- src/bot_logic/constant.py
+++ src/bot_logic/constant.py
@@ -2,12 +2,25 @@
 Defines constants for pinouts, channels, general configurations,
 and system messages
 '''
+from shared import pins
 
+# Configurations for pinouts
+ALERT_LED_PIN = 1  # Example, if we want to have an LED status light
 
-#Configurations for pinouts
-ALERT_LED_PIN = 1#Example, if we want to have an LED status light
+L_MOTOR_PWM_PIN = pins.PWM2B  # DC Motor pinout
+L_MOTOR_SEL_PIN = 'P8_14'
+R_MOTOR_PWM_PIN = pins.PWM2A
+R_MOTOR_SEL_PIN = 'P8_16'
+MOTOR_DRIVER_MODE_PIN = 'P8_18'
 
-#Alert Messages
+PAN_STEPPER_AIN_PIN = 'P9_15'  # Stepper for panning camera/sensor pinout
+PAN_STEPPER_BIN_PIN = 'P9_23'
+PAN_STEPPER_CIN_PIN = 'P9_25'
+PAN_STEPPER_DIN_PIN = 'P9_27'
 
-#Other configs
-UART_BAUD=9600
+# Alert Messages
+
+# Other configs
+SYSTEM_RESET_TIMEOUT_MS = 2000  # Time to wait for system to initialize
+UART_BAUD = 9600
+CAM_CAP_DELAY_MS = 20  # Delay between camera batch captures in ms
